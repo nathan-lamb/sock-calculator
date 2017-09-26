e3f3cf082cdb1869f2d9193a1b7e3efa6a814d85
Nathan Lamb <nathanlamb022@gmail.com>
2017-09-26 21:31:44 +1300

    Initial setup

---
+++ src/Measurements.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+class Measurements extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      footCircumference: [],
+      ankleCircumference: [],
+      gussetCircumference: [],
+      footLength: [],
+      lowerCalf: [],
+      heelDiagonal: [],
+      toeLength: []
+    }
+  }
+  render () {
+    return (
+      <div>
+        <h1>Measurements</h1>
+        <form>
+          Foot Circumference:
+          <input type="text"/><br></br>
+          Ankle Circumeference:
+          <input type="text"/><br></br>
+          Gusset Circumference:
+          <input type="text"/><br></br>
+          Foot Length:
+          <input type="text"/><br></br>
+          Lower Calf Circumference:
+          <input type="text"/><br></br>
+          Heel Diagonal:
+          <input type="text"/><br></br>
+          Toe Length:
+          <input type="text"/><br></br>
+          <input type="submit"/>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Measurements

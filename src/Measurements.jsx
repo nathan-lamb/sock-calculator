6d619551095bb623f1e48a20e51a8f81bf8aad94
Nathan Lamb <nathanlamb022@gmail.com>
2017-10-02 21:13:17 +1300

    Remove value attributes derp

--- src/Measurements.jsx
+++ src/Measurements.jsx
@@ -22,6 +22,7 @@ class Measurements extends React.Component {
     this.setState({
       [name]: value
     })
+    console.log(this.state)
   }
   render () {
     return (
@@ -29,19 +30,19 @@ class Measurements extends React.Component {
         <h1>Measurements</h1>
         <form onSubmit={this.handleSubmit}>
           Foot Circumference:
-          <input type="text" value={this.state.footCircumference} name="footCircumference"/><br></br>
+          <input type="text" name="footCircumference"/><br></br>
           Ankle Circumeference:
-          <input type="text" value={this.state.ankleCircumference} name="ankleCircumference"/><br></br>
+          <input type="text" name="ankleCircumference"/><br></br>
           Gusset Circumference:
-          <input type="text" value={this.state.gussetCircumference} name="gussetCircumference"/><br></br>
+          <input type="text" name="gussetCircumference"/><br></br>
           Foot Length:
-          <input type="text" value={this.state.footLength} name="footLength"/><br></br>
+          <input type="text" name="footLength"/><br></br>
           Lower Calf Circumference:
-          <input type="text" value={this.state.lowerCalf} name="lowerCalf"/><br></br>
+          <input type="text" name="lowerCalf"/><br></br>
           Heel Diagonal:
-          <input type="text" value={this.state.heelDiagonal} name="heelDiagonal"/><br></br>
+          <input type="text" name="heelDiagonal"/><br></br>
           Toe Length:
-          <input type="text" value={this.state.toeLength} name="toeLength"/><br></br>
+          <input type="text" name="toeLength"/><br></br>
           <input type="submit" value="Submit"/>
         </form>
       </div>
